e145d1f4b5e5ba7348fc7e7c6a6b1cb71f765b46
alxanderpierre <alxanderpierre@users.noreply.github.com>
2020-03-13 00:27:07 -0400

    updates

--- src/player.py
+++ src/player.py
@@ -1,2 +1,12 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, name, current_room):
+        self.name = name
+        self.current_room = current_room
+    def travel(self, direction):
+        if getattr(player.current_room, f"{direction}_to") is not None:
+            player.current_room = if getattr(player.current_room, f"{direction}_to")
+        else:
+            print("You can not move in that direction")
